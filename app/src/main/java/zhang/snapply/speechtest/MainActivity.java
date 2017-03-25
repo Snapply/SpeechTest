cf844af28991bcadc9b46e16e1c1ad62f7401631
Snapply Zhang <350624690@qq.com>
2017-03-25 17:01:35 +0800

    开始构建界面

--- app/src/main/java/zhang/snapply/speechtest/MainActivity.java
+++ app/src/main/java/zhang/snapply/speechtest/MainActivity.java
@@ -1,7 +1,14 @@
 package zhang.snapply.speechtest;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends Activity {
 
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+    }
 }
